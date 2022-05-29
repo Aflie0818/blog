b4985c480860997e3f5eea787495a650894f814f
Aflie0818 <iedzzz818@gmail.com>
2022-05-30 01:15:24 +0800

    Changes

--- blog/users/views.py
+++ blog/users/views.py
@@ -99,45 +99,47 @@ logger=logging.getLogger('django')
 class SmsCodeView(View):
 
     def get(self,request):
-        # 接收参数
+        # 1.接收参数
         image_code_client = request.GET.get('image_code')
         uuid = request.GET.get('uuid')
         mobile=request.GET.get('mobile')
 
-        # 校验参数：1.验证参数是否齐全
+        # 2.校验参数：
+        #     2.1验证参数是否齐全
         if not all([image_code_client, uuid,mobile]):
             return JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
-        # 2.图片验证码的验证
-        # 创建连接到redis的对象
+        #      2.图片验证码的验证
+        #          创建连接到redis的对象
         redis_conn = get_redis_connection('default')
-        # 提取图形验证码
+        #          提取图形验证码
         image_code_server = redis_conn.get('img:%s' % uuid)
+        #           判断图片验证码是否存在
         if image_code_server is None:
-            # 图形验证码过期或者不存在
+            #          图形验证码过期或者不存在
             return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
-        # 删除图形验证码，避免恶意测试图形验证码
+        #              删除图形验证码，避免恶意测试图形验证码
         try:
             redis_conn.delete('img:%s' % uuid)
         except Exception as e:
             logger.error(e)
-        # 对比图形验证码
+        #           对比图形验证码
         image_code_server = image_code_server.decode()  # bytes转字符串
         if image_code_client.lower() != image_code_server.lower():  # 转小写后比较
             return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
 
-        # 生成短信验证码：生成6位数验证码
+        # 3.生成短信验证码：生成6位数验证码
         sms_code = '%06d' % randint(0, 999999)
-        #将验证码输出在控制台，以方便调试
+        #     将验证码输出在控制台，以方便调试
         logger.info(sms_code)
-        # 保存短信验证码到redis中，并设置有效期
+        # 4.保存短信验证码到redis中，并设置有效期
         redis_conn.setex('sms:%s' % mobile, 300, sms_code)
-        # 发送短信验证码
+        # 5.发送短信验证码
         #   {1}短信验证码
         #   {2}短信验证码有效期
         # 参数3：免费开发测试使用的模板ID为1
         CCP().send_template_sms(mobile, [sms_code, 5],1)
 
-        # 响应结果
+        # 6.响应结果
         return JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
 
 
